c6b2c2b2204de0ac44dbe9b6d670ae494d9dff5b
gerald hernandez <gerald2557@gmail.com>
2016-05-09 20:06:04 -0700

    Converted project to use postgresql and installed gems for heroku deployment

--- Gemfile
+++ Gemfile
@@ -3,8 +3,6 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -38,8 +36,6 @@ group :development, :test do
 end
 
 group :development do
-  # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
@@ -82,3 +78,13 @@ gem 'delayed-web', github: 'thebestday/delayed-web'
 
 # easily add pagination to our list of posts
 gem 'will_paginate'
+
+# use postgresql
+gem 'pg'
+
+# gem needed for heroku deployment
+gem 'rails_12factor', group: :production
+
+
+# specified ruby version for heroku
+ruby "2.3.0"
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160505053326) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "connections", force: :cascade do |t|
     t.string   "provider"
     t.string   "oauth_token"
@@ -36,7 +39,7 @@ ActiveRecord::Schema.define(version: 20160505053326) do
     t.datetime "updated_at"
   end
 
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
@@ -67,7 +70,7 @@ ActiveRecord::Schema.define(version: 20160505053326) do
     t.boolean  "admin"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
